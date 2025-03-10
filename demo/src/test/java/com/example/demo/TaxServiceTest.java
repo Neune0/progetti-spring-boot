c1909e862ce8d27b77c0eac3a7c02d992a68712d
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-10 11:17:03 +0100

    altro commit

---
+++ demo/src/test/java/com/example/demo/TaxServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Service;
+@Service
+public class TaxServiceTest {
+
+    @Test
+    public void testCalculateTax() {
+        // test code
+    }
+}
